4e48a28e73c2de3eb43cf25641c37cdba826f74f
im100ruv <karnasaurabh123@gmail.com>
2025-01-30 19:14:31 +0530

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <main className="text-green-300">Hello World!</main>
+      <footer>react, vite, typescript, tailwind</footer>
+    </div>
+  )
+}
+
+export default App
